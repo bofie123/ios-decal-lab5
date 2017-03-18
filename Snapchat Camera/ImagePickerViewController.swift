c7563d7f815f3c786f919e4ac840bf264c66e1a8
Bofan Chen <bchen1@berkeley.edu>
2017-03-18 11:28:22 -0700

    done

--- Snapchat Camera/ImagePickerViewController.swift
+++ Snapchat Camera/ImagePickerViewController.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2017 org.iosdecal. All rights reserved.
 //
 
-// TODO: you'll need to import a library here
 import UIKit
+import AVFoundation
 
-// TODO: you'll need to edit this line to make your class conform to the AVCapturePhotoCaptureDelegate protocol
-class ImagePickerViewController: UIViewController {
+class ImagePickerViewController: UIViewController,AVCapturePhotoCaptureDelegate {
 
     @IBOutlet weak var imageViewOverlay: UIImageView!
     @IBOutlet weak var flipCameraButton: UIButton!
@@ -18,23 +17,80 @@ class ImagePickerViewController: UIViewController {
     @IBOutlet weak var sendImageButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     
+    // manages real time capture activity from input devices to create output media (photo/video)
+    let captureSession = AVCaptureSession()
+    
+    // the device we are capturing media from (i.e. front camera of an iPhone 7)
+    var captureDevice : AVCaptureDevice?
+    
+    // view that will let us preview what is being captured from the captureSession
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    // Object used to capture a single photo from our capture device
+    let photoOutput = AVCapturePhotoOutput()
+    
     // The image to send as a Snap
     var selectedImage = UIImage()
     
-    // TODO: add your instance methods for photo taking here
+    var currentPosition = AVCaptureDevicePosition.front
+    
     
     override func viewDidLoad() {
 
         super.viewDidLoad()
         
-        // TODO: call captureNewSession here
-        
+        captureNewSession(devicePostion: nil)
         toggleUI(isInPreviewMode: false)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         // hide the navigation bar while we are in this view
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    /// Creates a new capture session, and starts updating it using the user's
+    /// input device
+    ///
+    /// - Parameter devicePostion: location of user's camera - you'll need to figure out how to use this
+    func captureNewSession(devicePostion: AVCaptureDevicePosition?) {
+        
+        // specifies that we want high quality video captured from the device
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        if let deviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [AVCaptureDeviceType.builtInWideAngleCamera],
+                                                                        mediaType: AVMediaTypeVideo, position: AVCaptureDevicePosition.unspecified) {
+            
+            // Iterate through available devices until we find one that works
+            for device in deviceDiscoverySession.devices {
+                
+                // only use device if it supports video
+                if (device.hasMediaType(AVMediaTypeVideo)) {
+                    if (device.position == currentPosition) {
+                        
+                        captureDevice = device
+                        if captureDevice != nil {
+                            // Now we can begin capturing the session using the user's device!
+                            do {
+                                try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+                                
+                                if captureSession.canAddOutput(photoOutput) {
+                                    captureSession.addOutput(photoOutput)
+                                }
+                            }
+                            catch {
+                                print(error.localizedDescription)
+                            }
+                            
+                            if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) {                                 view.layer.addSublayer(previewLayer)
+                                previewLayer.frame = view.layer.frame
+                                captureSession.startRunning()
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,12 +105,20 @@ class ImagePickerViewController: UIViewController {
     
     
     @IBAction func takePhoto(_ sender: UIButton) {
-        // TODO: Replace the following code as per instructions in the spec.
-        // Instead of sending a squirrel pic every time, here we will want
-        // to start the process of creating a photo from our photoOutput
-        if let squirrelImage = UIImage(named: "squirrel") {
-            selectedImage = squirrelImage
-            toggleUI(isInPreviewMode: true)
+        let settingsForMonitoring = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with:settingsForMonitoring,
+                     delegate: self)
+    }
+    
+    /// Provides the delegate a captured image in a processed format (such as JPEG).
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        if let photoSampleBuffer = photoSampleBuffer {
+            // First, get the photo data using the parameters above
+            let photoData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer,previewPhotoSampleBuffer: previewPhotoSampleBuffer)
+                // Then use this data to create a UIImage, and set it equal to `selectedImage`
+            selectedImage = UIImage(data:photoData!)!
+                    // This method updates the UI so the send button appears (no need to edit it)
+                    toggleUI(isInPreviewMode: true)
         }
     }
     
@@ -64,8 +128,16 @@ class ImagePickerViewController: UIViewController {
     ///
     /// - Parameter sender: The flip camera button in the top left of the view
     @IBAction func flipCamera(_ sender: UIButton) {
-        // TODO: allow user to switch between front and back camera
-        // you will need to create a new session using 'captureNewSession'
+        for input in captureSession.inputs {
+            captureSession.removeInput(input as! AVCaptureInput)
+        }
+        captureSession.stopRunning()
+        if currentPosition == AVCaptureDevicePosition.front {
+            currentPosition = AVCaptureDevicePosition.back
+        } else {
+            currentPosition = AVCaptureDevicePosition.front
+        }
+        captureNewSession(devicePostion: currentPosition)
     }
 
     
